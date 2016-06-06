92a00456534c9f2ba2f0ab21c4b897827c4a276e
Richard <rick_pendlebury@hotmail.com>
2016-06-06 12:47:52 +0100

    Tidy DynamicHeightNetworkImageView

--- src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
+++ src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -2,7 +2,6 @@ package com.example.xyzreader.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 
 import com.android.volley.toolbox.NetworkImageView;
 
